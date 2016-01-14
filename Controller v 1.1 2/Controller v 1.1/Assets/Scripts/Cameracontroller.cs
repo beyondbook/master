fa5bb24b66a9b0185ade75c000b5fc78083e7e9a
alasofang <alasofang@gmail.com>
2016-01-14 11:19:22 +0100

    this is the second version

---
+++ Controller v 1.1 2/Controller v 1.1/Assets/Scripts/Cameracontroller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cameracontroller : MonoBehaviour {
+
+	public GameObject ThirdPersonController;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = ThirdPersonController.transform.position + offset;
+	}
+}
---
+++ Controller v 1.1 2/Controller v 1.1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+
+	void Start(){
+		moveSpeed = 30f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,moveSpeed*Input.GetAxis("UpDown")*Time.deltaTime,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime );
+	}
+}
